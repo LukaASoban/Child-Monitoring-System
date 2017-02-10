08936fb48699b4d5173c45e4a3406e7f2a83999d
kstrange62 <kstrange62@gmail.com>
2017-02-10 08:31:14 -0500

    UI updates Added menu

--- app/src/main/java/bigbrother/child_monitoring_system/HomeScreen.java
+++ app/src/main/java/bigbrother/child_monitoring_system/HomeScreen.java
@@ -1,13 +1,21 @@
 package bigbrother.child_monitoring_system;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,15 +26,37 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
     private Button buttonProfile;
     private String uid;
 
+    //menu test//
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle = "Home";
+    ////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_home_screen);
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
         uid = getIntent().getStringExtra("uid");
 
         buttonProfile = (Button) findViewById(R.id.profile);
 
         buttonProfile.setOnClickListener(this);
+
+        //menu test//
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+//        mActivityTitle = getTitle().toString();
+        addDrawerItems();
+        setupDrawer();
+        ////////////
     }
 
     @Override
@@ -35,4 +65,78 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
         welcomeScreenIntent.putExtra("uid", uid);
         startActivity(welcomeScreenIntent);
     }
+
+    //menu test//
+    private void addDrawerItems() {
+        String[] menuArr = getResources().getStringArray(R.array.parent_menu);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.menu_item, menuArr);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    final Intent homeScreenIntent = new Intent(HomeScreen.this, HomeScreen.class);
+                    homeScreenIntent.putExtra("uid", uid);
+                    startActivity(homeScreenIntent);
+                } else if (position == 2){
+                    final Intent profileScreenIntent = new Intent(HomeScreen.this, Profile.class);
+                    profileScreenIntent.putExtra("uid", uid);
+                    startActivity(profileScreenIntent);
+                } else {
+                    Toast.makeText(HomeScreen.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    ////////////
 }
--- app/src/main/java/bigbrother/child_monitoring_system/Profile.java
+++ app/src/main/java/bigbrother/child_monitoring_system/Profile.java
@@ -1,11 +1,21 @@
 package bigbrother.child_monitoring_system;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,10 +41,21 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
 
     private Button editButton;
 
+    //menu test//
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle = "Home";
+    ////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_profile);
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
         firstName = (TextView) findViewById(R.id.firstNameData);
         lastName = (TextView) findViewById(R.id.lastNameData);
         email = (TextView) findViewById(R.id.emailData);
@@ -71,6 +92,17 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
         });
 
         editButton.setOnClickListener(this);
+
+        //menu test//
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+//        mActivityTitle = getTitle().toString();
+        addDrawerItems();
+        setupDrawer();
+        ////////////
     }
 
 
@@ -83,6 +115,11 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
             childFirstName.setFocusableInTouchMode(true);
             childLastName.setFocusableInTouchMode(true);
             schoolName.setFocusableInTouchMode(true);
+            firstName.setBackground(getResources().getDrawable(R.drawable.textbox));
+            lastName.setBackground(getResources().getDrawable(R.drawable.textbox));
+            childFirstName.setBackground(getResources().getDrawable(R.drawable.textbox));
+            childLastName.setBackground(getResources().getDrawable(R.drawable.textbox));
+            schoolName.setBackground(getResources().getDrawable(R.drawable.textbox));
             editButton.setText("Save");
             //Toast.makeText(Profile.this, "Should be editable", Toast.LENGTH_SHORT).show();
         } else if (v == editButton && editButton.getText().equals("Save")) {
@@ -96,6 +133,11 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
             childLastName.setFocusable(false);
             schoolName.setFocusable(false);
             editButton.setText("Edit");
+            firstName.setBackground(getResources().getDrawable(R.drawable.empty));
+            lastName.setBackground(getResources().getDrawable(R.drawable.empty));
+            childFirstName.setBackground(getResources().getDrawable(R.drawable.empty));
+            childLastName.setBackground(getResources().getDrawable(R.drawable.empty));
+            schoolName.setBackground(getResources().getDrawable(R.drawable.empty));
             //Toast.makeText(Profile.this, "Should not be editable. Done updating", Toast.LENGTH_SHORT).show();
         } else {
 
@@ -139,4 +181,78 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
                 childLastName.getText().toString(), schoolName.getText().toString(), email.getText().toString());
         fdbUsers.child(uid).setValue(currUser);
     }
+
+    //menu test//
+    private void addDrawerItems() {
+        String[] menuArr = getResources().getStringArray(R.array.parent_menu);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.menu_item, menuArr);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    final Intent homeScreenIntent = new Intent(Profile.this, HomeScreen.class);
+                    homeScreenIntent.putExtra("uid", uid);
+                    startActivity(homeScreenIntent);
+                } else if (position == 2){
+                    final Intent profileScreenIntent = new Intent(Profile.this, Profile.class);
+                    profileScreenIntent.putExtra("uid", uid);
+                    startActivity(profileScreenIntent);
+                } else {
+                    Toast.makeText(Profile.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    ////////////
 }
