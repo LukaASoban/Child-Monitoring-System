b65c53864ea4c976dd1b0b413b543ee48289703e
Sophie <swodajo3@gmail.com>
2017-03-10 21:32:21 -0500

    admin is able to upload floor plan

--- app/src/main/java/bigbrother/child_monitoring_system/Map.java
+++ app/src/main/java/bigbrother/child_monitoring_system/Map.java
@@ -1,5 +1,6 @@
 package bigbrother.child_monitoring_system;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -16,14 +17,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
@@ -36,6 +40,8 @@ public class Map extends AppCompatActivity {
 
     private ImageView floorPlanIV;
     private StorageReference storage;
+    private Button uploadImageButton;
+    private static final int GALLERY_INTENT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +50,19 @@ public class Map extends AppCompatActivity {
 
 
         floorPlanIV = (ImageView) findViewById(R.id.floorPlanIV);
+        uploadImageButton = (Button) findViewById(R.id.uploadImageButton);
+
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
         storage = FirebaseStorage.getInstance().getReference();
 
-        storage.child("Daycare/daycare1.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        storage.child("Daycare/daycare2.png").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onSuccess(Uri uri) {
@@ -62,6 +78,36 @@ public class Map extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_INTENT && resultCode ==RESULT_OK) {
+            Uri uri = data.getData();
+
+            StorageReference filepath = storage.child("Daycare").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(Map.this, "Image Uploaded", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
