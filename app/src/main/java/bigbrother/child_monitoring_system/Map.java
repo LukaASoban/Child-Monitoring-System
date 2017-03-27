c2ef24086f107ff5238a0768e56dd9893904e7b8
kstrange62 <kstrange62@gmail.com>
2017-03-27 10:32:03 -0400

    Fixed menu on the Map screen.

--- app/src/main/java/bigbrother/child_monitoring_system/Map.java
+++ app/src/main/java/bigbrother/child_monitoring_system/Map.java
@@ -1,6 +1,7 @@
 package bigbrother.child_monitoring_system;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
@@ -8,11 +9,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -48,6 +56,16 @@ public class Map extends AppCompatActivity implements MapInputDialog.OnCompleteL
 
     private String currentRoomName, currentPiMAC;
 
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle = "Map";
+    private UserType userType;
+    private DatabaseReference dRef;
+    private String uid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +73,8 @@ public class Map extends AppCompatActivity implements MapInputDialog.OnCompleteL
         //create the database reference
         db = FirebaseDatabase.getInstance().getReference().child("daycare").child("Georgia Tech").child("mapdata");
         dbChildren = FirebaseDatabase.getInstance().getReference().child("daycare").child("Georgia Tech").child("children");
+        dRef = FirebaseDatabase.getInstance().getReference().child("users");
+        uid = getIntent().getStringExtra("uid");
         //load the rooms for the FloorView
         onLoad();
 
@@ -93,6 +113,14 @@ public class Map extends AppCompatActivity implements MapInputDialog.OnCompleteL
             }
         });
 
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        getSupportActionBar().setTitle(mActivityTitle);
+        addDrawerItems();
+        setupDrawer();
     }
 
     public void onLoad() {
@@ -195,5 +223,139 @@ public class Map extends AppCompatActivity implements MapInputDialog.OnCompleteL
                 }
             });
         }
+    }
+
+    private void addDrawerItems() {
+        dRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String[] menuArr = getResources().getStringArray(R.array.parent_menu);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (snapshot.getKey().equals(uid)) {
+                        User curUser = snapshot.getValue(User.class);
+                        userType = curUser.getType();
+                        if (curUser.getType().equals(UserType.ADMIN)) {
+                            menuArr = getResources().getStringArray(R.array.admin_menu);
+                        } else if (curUser.getType().equals(UserType.EMPLOYEE)){
+                            menuArr = getResources().getStringArray(R.array.employee_menu);
+                        }
+                    }
+                }
+                mAdapter = new ArrayAdapter<String>(Map.this, R.layout.menu_item, menuArr);
+                mDrawerList.setAdapter(mAdapter);
+                mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if (userType.equals(UserType.PARENT)) {
+                            if (position == 0) {
+                                final Intent homeScreenIntent = new Intent(Map.this, HomeScreen.class);
+                                homeScreenIntent.putExtra("uid", uid);
+                                startActivity(homeScreenIntent);
+                            } else if (position == 1) {
+                                final Intent mapScreenIntent = new Intent(Map.this, Map.class);
+                                mapScreenIntent.putExtra("uid", uid);
+                                startActivity(mapScreenIntent);
+                            } else if (position == 2) {
+                                final Intent profileScreenIntent = new Intent(Map.this, Profile.class);
+                                profileScreenIntent.putExtra("uid", uid);
+                                startActivity(profileScreenIntent);
+                            } else {
+                                Toast.makeText(Map.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (userType.equals(UserType.ADMIN)) {
+                            if (position == 0) {
+                                final Intent homeScreenIntent = new Intent(Map.this, HomeScreen.class);
+                                homeScreenIntent.putExtra("uid", uid);
+                                startActivity(homeScreenIntent);
+                            } else if (position == 1) {
+                                final Intent mapScreenIntent = new Intent(Map.this, Map.class);
+                                mapScreenIntent.putExtra("uid", uid);
+                                startActivity(mapScreenIntent);
+                            } else if (position == 2) {
+                                final Intent searchScreenIntent = new Intent(Map.this, SearchScreen.class);
+                                searchScreenIntent.putExtra("uid", uid);
+                                startActivity(searchScreenIntent);
+                            } else if (position == 3){
+                                final Intent profileScreenIntent = new Intent(Map.this, Profile.class);
+                                profileScreenIntent.putExtra("uid", uid);
+                                startActivity(profileScreenIntent);
+                            } else {
+                                Toast.makeText(Map.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (userType.equals(UserType.EMPLOYEE)) {
+                            if (position == 0) {
+                                final Intent homeScreenIntent = new Intent(Map.this, HomeScreen.class);
+                                homeScreenIntent.putExtra("uid", uid);
+                                startActivity(homeScreenIntent);
+                            } else if (position == 1) {
+                                final Intent mapScreenIntent = new Intent(Map.this, Map.class);
+                                mapScreenIntent.putExtra("uid", uid);
+                                startActivity(mapScreenIntent);
+                            } else if (position == 3){
+                                final Intent profileScreenIntent = new Intent(Map.this, Profile.class);
+                                profileScreenIntent.putExtra("uid", uid);
+                                startActivity(profileScreenIntent);
+                            } else {
+                                Toast.makeText(Map.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError firebaseError) {
+
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
