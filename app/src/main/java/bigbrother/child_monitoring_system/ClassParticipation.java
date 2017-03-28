4a2afc951e66f61006a4e2f6ca003a70a38d1507
kstrange62 <kstrange62@gmail.com>
2017-03-28 14:05:06 -0400

    Added screen for allowing teachers to send notifications for parents. Still needs to be connected.

---
+++ app/src/main/java/bigbrother/child_monitoring_system/ClassParticipation.java
@@ -0,0 +1,186 @@
+package bigbrother.child_monitoring_system;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ClassParticipation extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle = "Map";
+    private UserType userType;
+    private DatabaseReference dRef;
+    private String uid;
+    private DatabaseReference dbChildren;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dbChildren = FirebaseDatabase.getInstance().getReference().child("daycare").child("Georgia Tech").child("children");
+        dRef = FirebaseDatabase.getInstance().getReference().child("users");
+        uid = getIntent().getStringExtra("uid");
+        setContentView(R.layout.activity_class_participation);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        getSupportActionBar().setTitle(mActivityTitle);
+        addDrawerItems();
+        setupDrawer();
+    }
+
+    private void addDrawerItems() {
+        dRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String[] menuArr = getResources().getStringArray(R.array.parent_menu);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (snapshot.getKey().equals(uid)) {
+                        User curUser = snapshot.getValue(User.class);
+                        userType = curUser.getType();
+                        if (curUser.getType().equals(UserType.ADMIN)) {
+                            menuArr = getResources().getStringArray(R.array.admin_menu);
+                        } else if (curUser.getType().equals(UserType.EMPLOYEE)){
+                            menuArr = getResources().getStringArray(R.array.employee_menu);
+                        }
+                    }
+                }
+                mAdapter = new ArrayAdapter<String>(ClassParticipation.this, R.layout.menu_item, menuArr);
+                mDrawerList.setAdapter(mAdapter);
+                mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if (userType.equals(UserType.PARENT)) {
+                            if (position == 0) {
+                                final Intent homeScreenIntent = new Intent(ClassParticipation.this, HomeScreen.class);
+                                homeScreenIntent.putExtra("uid", uid);
+                                startActivity(homeScreenIntent);
+                            } else if (position == 1) {
+                                final Intent mapScreenIntent = new Intent(ClassParticipation.this, Map.class);
+                                mapScreenIntent.putExtra("uid", uid);
+                                startActivity(mapScreenIntent);
+                            } else if (position == 2) {
+                                final Intent profileScreenIntent = new Intent(ClassParticipation.this, Profile.class);
+                                profileScreenIntent.putExtra("uid", uid);
+                                startActivity(profileScreenIntent);
+                            } else {
+                                Toast.makeText(ClassParticipation.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (userType.equals(UserType.ADMIN)) {
+                            if (position == 0) {
+                                final Intent homeScreenIntent = new Intent(ClassParticipation.this, HomeScreen.class);
+                                homeScreenIntent.putExtra("uid", uid);
+                                startActivity(homeScreenIntent);
+                            } else if (position == 1) {
+                                final Intent mapScreenIntent = new Intent(ClassParticipation.this, Map.class);
+                                mapScreenIntent.putExtra("uid", uid);
+                                startActivity(mapScreenIntent);
+                            } else if (position == 2) {
+                                final Intent searchScreenIntent = new Intent(ClassParticipation.this, SearchScreen.class);
+                                searchScreenIntent.putExtra("uid", uid);
+                                startActivity(searchScreenIntent);
+                            } else if (position == 3){
+                                final Intent profileScreenIntent = new Intent(ClassParticipation.this, Profile.class);
+                                profileScreenIntent.putExtra("uid", uid);
+                                startActivity(profileScreenIntent);
+                            } else {
+                                Toast.makeText(ClassParticipation.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (userType.equals(UserType.EMPLOYEE)) {
+                            if (position == 0) {
+                                final Intent homeScreenIntent = new Intent(ClassParticipation.this, HomeScreen.class);
+                                homeScreenIntent.putExtra("uid", uid);
+                                startActivity(homeScreenIntent);
+                            } else if (position == 1) {
+                                final Intent mapScreenIntent = new Intent(ClassParticipation.this, Map.class);
+                                mapScreenIntent.putExtra("uid", uid);
+                                startActivity(mapScreenIntent);
+                            } else if (position == 3){
+                                final Intent profileScreenIntent = new Intent(ClassParticipation.this, Profile.class);
+                                profileScreenIntent.putExtra("uid", uid);
+                                startActivity(profileScreenIntent);
+                            } else {
+                                Toast.makeText(ClassParticipation.this, "Not setup yet!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError firebaseError) {
+
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
